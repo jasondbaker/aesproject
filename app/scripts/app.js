6da8e5c05f10949dfcc5173c2d189085ea83abd4
Jason Baker <jason.baker@enstratius.com>
2015-11-01 19:55:18 -0600

    angular site initial yeoman build

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aesApp
+ * @description
+ * # aesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aesApp', ['ngRoute']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aesApp
+ */
+angular.module('aesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
