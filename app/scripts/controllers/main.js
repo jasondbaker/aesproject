932932eae88077d0f08409ac976e99339d0dc9bf
Jason Baker <jason.baker@enstratius.com>
2015-11-02 00:22:05 -0600

    string to hex conversion functions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,28 @@
  */
 angular.module('aesApp')
   .controller('MainCtrl', ['$scope', 'aes', function ($scope, aes) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.message = 'Hello Big World!'; // Hello Big World!
+
+    var stringToHexArray = function (s) {
+      var hex = [];
+      for (var i=0; i < s.length; i++) {
+        hex.push(s.charCodeAt(i).toString(16).toUpperCase());
+      }
+      return hex;
+    };
+
+    var hexArrayToString = function (h) {
+      var str = '';
+      for (var i=0; i < h.length; i++) {
+        str = str + String.fromCharCode(parseInt(h[i], 16));
+      }
+      return str;
+    };
+
+    $scope.byteMessage = stringToHexArray($scope.message);
+
+    $scope.convMessage = hexArrayToString($scope.byteMessage);
 
     $scope.foo = aes.test(1);
 
