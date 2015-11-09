d4aef8d8cd11d3d6e6f2ddec98aa22fccd2f1515
Jason Baker <jason.baker@enstratius.com>
2015-11-08 22:10:38 -0600

    update encryption form interface

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,31 +12,61 @@ angular.module('aesApp')
    function ($scope, aes, convert) {
 
     $scope.encryption = {
-      plaintext: undefined,
-      key : undefined,
-      ciphertext : undefined
+      plaintext: {
+        value : undefined,
+        type : 'ascii'
+      },
+      key : {
+        value : undefined,
+        type: 'ascii'
+      },
+      ciphertext : {
+        value : undefined,
+        type : 'hex'
+      }
     };
 
     $scope.encrypt = function(plaintext, key){
       var size;
+      var thePlaintext = [], theKey = [];
 
       // convert the input strings into arrays
-      plaintext = convert.stringToArray(plaintext);
-      key = convert.stringToArray(key);
+      if (plaintext.type === 'ascii') {
+        thePlaintext = convert.stringToArray(plaintext.value);
+      } else {
+        thePlaintext = convert.hexToArray(plaintext.value);
+      }
+      if (key.type === 'ascii') {
+        theKey = convert.stringToArray(key.value);
+      } else {
+        theKey = convert.hexToArray(key.value);
+      }
 
       // set the proper byte-size for the key
-      if (key.length < 17) {size = 16;}
-      else if (key.length < 25) {size = 24;}
+      if (key.value.length < 17) {size = 16;}
+      else if (key.value.length < 25) {size = 24;}
       else {size = 32;}
 
-      key = aes.parseKey(key,size);
-      $scope.encryption.ciphertext = convert.arrayToHexString(aes.encrypt(plaintext, key), true);
+      theKey = aes.parseKey(theKey,size);
+      $scope.encryption.ciphertext.value = convert.arrayToHexString(aes.encrypt(thePlaintext, theKey), true);
     };
 
     $scope.clearEncrypt = function(){
-      $scope.encryption.ciphertext = '';
-      $scope.encryption.plaintext = '';
-      $scope.encryption.key = '';
+      $scope.encryption.ciphertext.value = '';
+      $scope.encryption.plaintext.value = '';
+      $scope.encryption.key.value = '';
     };
+
+    // set the type of an input string to ascii or hex
+    $scope.setType = function(direction,input,type){
+      $scope[direction][input].type = type;
+      $scope[direction][input].value = '';
+      if (direction === 'encryption') {
+        $scope[direction].ciphertext.value = '';
+      } else {
+        $scope[direction].plaintext.value = '';
+      }
+    };
+
 
 }]);
